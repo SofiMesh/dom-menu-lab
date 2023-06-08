09866d025ba70e1935297c78fbd451eab8554f36
SofiMesh <sofimeshvildishvili@gmail.com>
2023-06-08 16:16:43 -0400

    Task 2.0 done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 console.log("Hello world")
 const mainEl = document.querySelector('main)');
 mainEl.style.backgroundColor = 'var(--main-bg)';
-mainEl.innerHTML= '<h1>SEI Rocks!</h1>';
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
 mainEl.classList.add("flex-ctr");
+var topMenuEl = document.getElementById("top-menu");
