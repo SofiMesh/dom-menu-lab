dc1a0dad77261856f8b083abc282486826b76c59
SofiMesh <sofimeshvildishvili@gmail.com>
2023-06-08 16:11:06 -0400

    Task 1.3 done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 console.log("Hello world")
 const mainEl = document.querySelector('main)');
 mainEl.style.backgroundColor = 'var(--main-bg)';
-mainEl.classList.add('flex-ctr');
+mainEl.innerHTML= '<h1>SEI Rocks!</h1>';
+mainEl.classList.add("flex-ctr");
