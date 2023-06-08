fb7f59adfb9a109cec5956f29e7e31ef04587b5f
SofiMesh <sofimeshvildishvili@gmail.com>
2023-06-08 16:19:24 -0400

    Task 2.1 done

--- js/app.js
+++ js/app.js
@@ -4,3 +4,4 @@ mainEl.style.backgroundColor = 'var(--main-bg)';
 mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
 mainEl.classList.add("flex-ctr");
 var topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = '100%'
