bde83654e18b74c379d399dd8c7f04ab495ceaae
SofiMesh <sofimeshvildishvili@gmail.com>
2023-06-08 16:25:19 -0400

    Task 2.3 done

--- js/app.js
+++ js/app.js
@@ -6,3 +6,4 @@ mainEl.classList.add("flex-ctr");
 var topMenuEl = document.getElementById("top-menu");
 topMenuEl.style.height = '100%'
 topMenuEl.style.backgroundColor = '--top-menu-bg';
+topMenuEl.classList.add('flex-around');
